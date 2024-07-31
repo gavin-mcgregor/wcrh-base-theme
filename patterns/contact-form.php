453ecde40b501913efde87c1d6f989d261d8d1e3
Gavin <gurven-mckruger@hotmail.co.uk>
2024-07-31 16:25:40 +0100

    Update custom icons, new patterns

---
+++ patterns/contact-form.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Title: Contact
+ * Slug: wcrh-base-theme/contact-form
+ * Categories: wcrh
+ */
+?>
+
+<!-- wp:group {"tagName":"section","metadata":{"name":"Section"},"backgroundColor":"base","layout":{"type":"default"},"fullHeight":true,"centerVert":true} -->
+<section class="wp-block-group has-base-background-color has-background"><!-- wp:group {"metadata":{"name":"Inner"},"layout":{"type":"default"},"width":"page-width-normal"} -->
+    <div class="wp-block-group"><!-- wp:wcrh/custom-spacer /-->
+
+        <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"},"width":"page-width-text"} -->
+        <div class="wp-block-group"><!-- wp:heading -->
+            <h2 class="wp-block-heading">Get in Touch</h2>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph -->
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec posuere quis eros eget pulvinar. Sed eu ultricies elit, eu aliquam metus. Nunc consectetur est non tempus mattis. Quisque hendrerit urna nec ipsum suscipit porta. Suspendisse imperdiet velit nec nibh auctor, eget elementum lacus sollicitudin. Maecenas in massa id felis dapibus pharetra.</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:wcrh/custom-spacer {"height":"spacer-xhalf"} /-->
+
+            <!-- wp:wcrh/custom-form /-->
+        </div>
+        <!-- /wp:group -->
+
+        <!-- wp:wcrh/custom-spacer {"height":"spacer-x2"} /-->
+    </div>
+    <!-- /wp:group -->
+</section>
+<!-- /wp:group -->
